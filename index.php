43f91d753bf76e52c34b706dfa02089a450f75cc
ZakariaBj <zakariabenjeddi22@gmail.com>
2024-10-14 12:52:20 +0100

    connextion avec la base de donne

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+        function redirigerVersAutrePage() {
+            window.location.href = "pages/sign-in.php";
+        }
+        setTimeout(redirigerVersAutrePage,500);
+    </script>
+</head>
+<body>
+</html>
